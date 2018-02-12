a43fcb45997bf9bdceecc5dbda8b4facb4734293
krystianl <krystian.leszczewski@nask.pl>
2018-02-12 14:56:53 +0100

    Hashing to Block class added and some nodes examples added

---
+++ src/pl/krystiano/NoobChain.java
@@ -0,0 +1,20 @@
+package pl.krystiano;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoobChain {
+
+    public static void main(String[] args) {
+
+        Block genesisBlock = new Block("Hi im the first block", "0");
+        System.out.println("Hash for block 1 : " + genesisBlock.hash);
+
+        Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
+        System.out.println("Hash for block 2 : " + secondBlock.hash);
+
+        Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
+        System.out.println("Hash for block 3 : " + thirdBlock.hash);
+
+    }
+}
